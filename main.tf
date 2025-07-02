77881a4b0c82186a7ff1b0cf0505c3609ea9cc4d
denisvizi <139752996+denisvizi@users.noreply.github.com>
2025-07-02 10:12:26 +0300

    load balancer4

--- main.tf
+++ main.tf
@@ -52,13 +52,18 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   }
 
