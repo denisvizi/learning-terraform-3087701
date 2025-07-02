b06b3e7ade9244f8865b7e2caef3601bf236f174
denisvizi <139752996+denisvizi@users.noreply.github.com>
2025-07-02 10:37:01 +0300

    loadbalancer

--- main.tf
+++ main.tf
@@ -16,12 +16,12 @@ data "aws_ami" "app_ami" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -35,8 +35,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
+  subnet_id             = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -44,14 +43,15 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.17.0"
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {  # Note: This is now a map, not a list
+  target_groups = {
     blog-tg = {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
@@ -60,13 +60,13 @@ module "alb" {
     }
   }
 
-   listeners = {
+  listeners = {
     http = {
       port     = 80
       protocol = "HTTP"
       default_action = {
         type             = "forward"
-        target_group_key = "blog-tg
+        target_group_key = "blog-tg"
       }
     }
   }
@@ -77,7 +77,7 @@ module "alb" {
 }
 
 resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = module.alb.target_group_arns[0]
+  target_group_arn = module.alb.target_groups["blog-tg"].arn
   target_id        = aws_instance.blog.id
   port             = 80
 }
@@ -88,9 +88,8 @@ module "blog_sg" {
   name    = "blog"
 
   vpc_id              = module.blog_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
