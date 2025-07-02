27da1b0a13d82e8c17b11c17757940e1d91d413a
denisvizi <139752996+denisvizi@users.noreply.github.com>
2025-07-02 11:51:21 +0300

    autoscaling

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns  # Correct output name
+  target_group_arbs   = module.blog_alb.target_group_arns  # Correct output name
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
