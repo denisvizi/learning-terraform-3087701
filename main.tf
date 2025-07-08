4ecae450a5fe6660c500144dfeadaad1a1b9450c
denisvizi <139752996+denisvizi@users.noreply.github.com>
2025-07-08 16:31:04 +0300

    variables

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = "9.0.1"
 
   name = "${var.environment.name}-blog"
 
